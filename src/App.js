8bf0625a80ac28ea080928aae8adbcfc59a3d7f1
adrian <xwrobel02@gmail.com>
2023-01-13 21:57:42 +0100

    add fetching items from api

--- src/App.js
+++ src/App.js
@@ -3,34 +3,54 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import SearchItems from './SearchItem';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const API_URL = 'http://localhost:3001/items';
 
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  function setAndSaveItems(newItems) {
-    setItems(newItems);
-    localStorage.setItem('shoppingList', JSON.stringify(newItems));
-  }
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null)
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    // Simulation of slow API
+    // setTimeout(() => {
+    //   fetchItems()
+    // }, 2000)
+    fetchItems() // or (async () => await fetchItems())();
+
+  }, [])
 
   function addNewItem(item) {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   function handleCheck(id) {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   function handleDelete(id) {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   function handleSubmit(e) {
@@ -52,11 +72,15 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/SearchItem.js
+++ src/SearchItem.js
@@ -1,6 +1,6 @@
 const SearchItems = ({ search, setSearch }) => {
     return (
-        <form className="searchForm" onSubmit={(e) => e.preventDefault}>
+        <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
             <label htmlFor="search">Search</label>
             <input
                 id="search"
